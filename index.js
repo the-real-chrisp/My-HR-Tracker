f1a3cf7b264cfacb451eca2b0514aff38ea1b1b7
Chris Purcell <chrisp0000@gmail.com>
2023-12-10 17:19:56 -0800

    Created connection to MySQL. Set up initial prompt to direct users to their desired action. Initialized node. Completed schema.sql to match acceptance criteria.

--- index.js
+++ index.js
@@ -9,3 +9,27 @@ const connection = mysql.createConnection({
     database: "employees_db"
 });
 
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to database!');
+
+    beginPrompt();
+})
+
+function beginPrompt() {
+    inquirer
+        .prompt({
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'direct',
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update an employee role"
+            ]
+        });
+};
--- schema.sql
+++ schema.sql
@@ -1,8 +1,33 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db:
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
-USE employee_db;
+USE employees_db;
 
 CREATE TABLE department (
-    id INT NOT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(8,2),
+    department_id INT,
+    PRIMARY KEY (id)
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id)
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
 )
