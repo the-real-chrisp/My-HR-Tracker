13e1273ecc36cb972b3875d25f06e57747ab6b5d
Chris Purcell <chrisp0000@gmail.com>
2023-12-10 16:37:38 -0800

    added in mysql connection

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employees_db"
+});
+
