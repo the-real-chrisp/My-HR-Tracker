a715e64ec0d567161c80d030b10f189302509b89
Chris Purcell <chrisp0000@gmail.com>
2023-12-10 18:07:58 -0800

    Added in switch case to handle user selection for first prompt.

--- index.js
+++ index.js
@@ -31,5 +31,31 @@ function beginPrompt() {
                 "Add an employee",
                 "Update an employee role"
             ]
+        })
+        .then((answers) => {
+            switch (answers.direct) {
+                case "View all departments":
+                    viewDepartments();
+                    break;
+                case "View all roles":
+                    viewRoles();
+                    break;
+                case "View all employees":
+                    viewEmployees();
+                    break;
+                case "Add a department":
+                    addDepartment();
+                    break;
+                case "Add a role":
+                    addRole();
+                    break;
+                case "Add an employee":
+                    addEmployee();
+                    break;
+                case "Update an employee role":
+                    updateEmployeeRole();
+                    break;
+            };
         });
 };
+
